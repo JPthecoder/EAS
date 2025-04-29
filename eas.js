f27a713a268b5dbce60a76d68192cc35b7f2f735
Johnson Phan <175180662+JPthecoder@users.noreply.github.com>
2025-04-29 08:06:49 -0700

    Set up proper layout for one div - adding 16 more

--- eas.js
+++ eas.js
@@ -2,19 +2,19 @@ const container = document.createElement("div")
 container.setAttribute("id", "main")
 document.body.appendChild(container);
 
-/*const div1 = document.createElement("div")
+const div1 = document.createElement("div")
 div1.setAttribute("id", "square")
 div1.textContent = "Box";
-container.appendChild(div1);*/
+container.appendChild(div1);
 
 const canvas = document.createElement("canvas")
-canvas.setAttribute("id", "canvas1")
-canvas.width = 560;
-canvas.height = 360;
-document.body.appendChild(canvas)
+canvas.setAttribute("id", "canvas")
+canvas.width = 160;
+canvas.height = 160;
+container.appendChild(canvas)
 console.log(canvas.width, canvas.height)
 
-/* div1.addEventListener("mousemove", newColor);
+div1.addEventListener("mousemove", newColor);
 
  function newColor(e){
     let target = e.target;
@@ -23,12 +23,12 @@ console.log(canvas.width, canvas.height)
     div1.style.backgroundColor = "lightblue";
 
 }
-}; */
+};
 function draw(){
     let isDrawing = false;
     let x = 0;
     let y = 0;
-    const myCanvas = document.getElementById("canvas1");
+    const myCanvas = document.getElementById("canvas");
     const context = myCanvas.getContext("2d");
 
     myCanvas.addEventListener("mouseover", (e)=>{
@@ -65,47 +65,4 @@ function draw(){
     }
 
 };
-
 draw();
-
-function drawAgain(){
-    let isDrawing = false;
-    let x = 0;
-    let y = 0;
-    const myCanvas = document.querySelector("canvas");
-    const context = myCanvas.getContext("2d");
-
-    myCanvas.addEventListener("mouseover", (e)=>{
-        x = e.offsetX;
-        y = e.offsetY;
-        isDrawing = true;
-    });
-
-    myCanvas.addEventListener("mousemove", (e) => {
-        if (isDrawing){
-            drawLine(context, x, y, e.offsetX, e.offsetY);
-        x = e.offsetX;
-        y = e.offsetY;
-        }
-    });
-
-    window.addEventListener("mouseleave", (e)=> {
-        if (isDrawing){
-            drawLine(context, x, y, e.offsetX, e.offsetY);
-            x = 0;
-            y = 0;
-            isDrawing = false;
-        }
-    });
-
-    function drawLine(context, x1, y1, x2, y2){
-        context.beginPath();
-        context.strokeStyle = "black";
-        context.lineWidth = "1";
-        context.moveTo(x1,y1);
-        context.lineTo(x2,y2);
-        context.stroke();
-        context.closePath();
-    }
-};
-drawAgain();
