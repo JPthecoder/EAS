30a4746b2402ad2e5576310a2d8f24106c47efc2
Johnson Phan <175180662+JPthecoder@users.noreply.github.com>
2025-04-30 15:22:18 -0700

    Trying to link eventlistener to my canvas however issue with interaction with get context for my canvas

--- eas.js
+++ eas.js
@@ -9,14 +9,20 @@ container.appendChild(div1);
 
 function createCanvas(){
 const canvas = document.createElement("canvas")
-canvas.setAttribute("id", "canvas")
+canvas.setAttribute("id", "myCanvas")
 canvas.width = 160;
 canvas.height = 160;
 container.appendChild(canvas)
 console.log(canvas.width, canvas.height)
 };
 
-createCanvas();
+const canvas1 = document.createElement("canvas")
+canvas1.setAttribute("id", "canvas1")
+canvas1.width = 160;
+canvas1.height = 160;
+container.appendChild(canvas1)
+console.log(canvas1.width, canvas1.height)
+
 
 window.addEventListener("mouseleave", (e)=> {
         if (isDrawing){
@@ -27,7 +33,7 @@ window.addEventListener("mouseleave", (e)=> {
         }
     });
 
-div1.addEventListener("mousemove", newColor);
+container.addEventListener("mousemove", newColor);
 
  function newColor(e){
     let target = e.target;
@@ -38,18 +44,57 @@ div1.addEventListener("mousemove", newColor);
 }
 };
 
-function draw(){
+function massCreate(){
+    for (i=0; i<15; i++){
+        createCanvas();
+        //draw() is only executing on one div instead of the 16 created
+    }
+};
+massCreate();
+
+
     let isDrawing = false;
     let x = 0;
     let y = 0;
-    const myCanvas = document.getElementById("canvas");
+    //const myCanvas = document.getElementsByTagName("myCanvas");
+
+    const myCanvas = document.querySelector('myCanvas');
     const context = myCanvas.getContext("2d");
 
+    container.addEventListener("mouseover", (e) =>{
+        let target = e.target;
+
+        if(target.id === 'myCanvas'){
+            myCanvas.addEventListener("mouseover", (e)=>{
+                x = e.offsetX;
+                y = e.offsetY;
+                isDrawing = true;
+            }); 
+        
+            myCanvas.addEventListener("mousemove", (e) => {
+                if (isDrawing){
+                    drawLine(context, x, y, e.offsetX, e.offsetY);
+                x = e.offsetX;
+                y = e.offsetY;
+                }
+            });
+        
+            window.addEventListener("mouseleave", (e)=> {
+                if (isDrawing){
+                    drawLine(context, x, y, e.offsetX, e.offsetY);
+                    x = 0;
+                    y = 0;
+                    isDrawing = false;
+                }
+            })
+        }
+    })
+    /*
     myCanvas.addEventListener("mouseover", (e)=>{
         x = e.offsetX;
         y = e.offsetY;
         isDrawing = true;
-    });
+    }); 
 
     myCanvas.addEventListener("mousemove", (e) => {
         if (isDrawing){
@@ -66,7 +111,7 @@ function draw(){
             y = 0;
             isDrawing = false;
         }
-    });
+    }) */
 
     function drawLine(context, x1, y1, x2, y2){
         context.beginPath();
@@ -78,14 +123,16 @@ function draw(){
         context.closePath();
     }
 
-};
-draw();
 
-function massCreate(){
-    for (i=0; i<15; i++){
-        createCanvas();
+
+
+/*
+container.addEventListener("mousemove", drawMe());
+
+ function drawMe(e){
+    let target = e.target;
+
+    if (target.id === "canvas"){
         draw();
-        //draw() is only executing on one div instead of the 16 created
     }
-};
-massCreate();
+}; */
