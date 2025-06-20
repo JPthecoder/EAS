3d4597530e46ca3b31c3aa82379643c9c29098f7
JPthecoder <175180662+JPthecoder@users.noreply.github.com>
2025-06-19 21:47:35 -0700

    Added a limit to the prompt

--- eas.js
+++ eas.js
@@ -18,15 +18,15 @@ function createSquares(size){
     for (i = 0; i<size * size; i++){
         const square = document.createElement("div");
         square.id = "square";
-        square.style.width = "160px";
-        square.style.height = "160px";
+        square.style.width = "16px";
+        square.style.height = "16px";
         container.appendChild(square)
     
         square.addEventListener("mouseover", ()=> {
         square.style.backgroundColor = "red" 
     });
 }};
-createSquares(4);
+createSquares(16);
 
 
 // create another div above the drawings container xx
@@ -39,17 +39,21 @@ let question = window.prompt("How many squares would you like to make?");
 if (question === null){
     alert("Cancelled")
 } else if (isNaN(question)){
-    alert("Error: choose a number") // prompt is create but still need to figure out how to do null answers and connect prompt to page reset
+    alert("Error: choose a number less than 100") // prompt is create but still need to figure out how to do null answers and connect prompt to page reset
 } else if (question === ""){
     alert("No answer: Input a number")
 } else {
     let size = Number(question);
-    container.innerHTML = "";
-    createSquares(size);
+        if (size > 100){
+            alert("Error: Enter a number less than 100")
+        } else {
+            container.innerHTML = "";
+            createSquares(size);
         //my call stack might be presenting inability to call my function
         //do I need to create a separate function to hold my command?
             //my page is showing up blank because the arugment is different from my (question) and (size)
-    console.log(createSquares())
+            console.log(createSquares())
+        }
     // need to figure out how to mesh createSquares after clearing the innerHTML
 }};
 // Have the answer of the user reflect the new number of squares on the page
